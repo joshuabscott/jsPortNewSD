f2e7f7c5c691e8a2052395aa70e7a9981a55bd61
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2020-10-31 12:22:24 -0700

    Contact form
    trying to figure out why my contact form does not work any more

---
+++ Scripts/my_js.js
@@ -0,0 +1,25 @@
+﻿const btn = document.getElementById('contact_button');
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.value = 'Sending...';
+
+        const serviceID = 'contact_service';
+        const templateID = 'contact_form';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                //alert('Sent!');
+                swal(
+                    'Good news',
+                    'Your email has been sent!',
+                    'success'
+                );
+                this.reset();
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+    });
